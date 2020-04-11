46aab3420f4ca108aabf8412348d2bde018a9dbe
YUKIKO IKEDA <applelele@gmail.com>
2020-04-11 14:14:55 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -6,6 +6,8 @@ class Appointment
     @@all
   end
 
+  attr_accessor :date, :doctor, :patient
+
   def initialize(date, doctor, patient)
     @date = date  #.strtime("%A, %m %d")
     @doctor = doctor
