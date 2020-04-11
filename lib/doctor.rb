af7c7f255d0eb939ee0bfa895bd1c19ccf83f2b6
YUKIKO IKEDA <applelele@gmail.com>
2020-04-11 13:52:07 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,26 @@
+class Doctor
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, self, patient)
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+
+  def patients
+    self.appointments.collect {|appointment| appointment.patient}
+  end
+end
